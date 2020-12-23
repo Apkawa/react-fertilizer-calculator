cddaee81bc67370f49d69df577e0224cdc337ddf
apkawa <apkawa@gmail.com>
2020-12-23 19:33:01 +0300

    Добавлена возможность задавать объем кратный 0,05л

--- src/components/Calculator/Options/Solution.tsx
+++ src/components/Calculator/Options/Solution.tsx
@@ -2,7 +2,7 @@ import React, {FunctionComponent} from "react";
 import {Card, Flex, Heading} from "rebass";
 import {Label} from "@rebass/forms";
 import {Input} from "../../ui/ReduxForm/Input";
-import {decimal, number} from "../../ui/ReduxForm/normalizers";
+import {decimal} from "../../ui/ReduxForm/normalizers";
 import {useSelector} from "react-redux";
 import {getFormValues} from "redux-form";
 import {REDUX_FORM_NAME} from "@/components/Calculator/constants";
@@ -27,10 +27,10 @@ export const Solution: FunctionComponent<SolutionVolumeProps> = () => {
             name="solution_volume"
             width="4rem"
             type="number"
-            step="1"
-            min="1"
-            pattern="^\d+$"
-            normalize={number}
+            step="0.05"
+            min="0.1"
+            max="100"
+            normalize={decimal}
             style={{
               textAlign: "center"
             }}
@@ -46,7 +46,7 @@ export const Solution: FunctionComponent<SolutionVolumeProps> = () => {
             type="number"
             step="0.01"
             min="0"
-            max="2000"
+            max="5000"
             normalize={decimal}
             style={{
               textAlign: "center"
